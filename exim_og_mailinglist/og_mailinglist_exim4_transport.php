b10b11c83438e7a0ead328a1a017fe0cb969a3db
Kyle Mathews <mathews.kyle@gmail.com>
2010-12-10 05:48:15 +0000

    remove og_mailinglist.drush.inc as we don't use it anymore

--- exim_og_mailinglist/og_mailinglist_exim4_transport.php
+++ exim_og_mailinglist/og_mailinglist_exim4_transport.php
@@ -1,7 +1,7 @@
 #!/usr/bin/php
 <?php
 
-// Reads in raw email off the STDIN and posts the email using our drush command on the appropriate site.
+// Reads in raw email off the STDIN and posts the email using our curl command for the appropriate site.
 
 // Grab the raw email message from stdin.
 $fd = fopen("php://stdin", "r");
@@ -9,22 +9,42 @@ while (!feof($fd)) {
   $raw_email .= fread($fd, 1024);
 }
 
-// Load site aliases
-require_once('site_aliases.php');
-$sites = og_mailinglist_site_aliases();
-
 // Set command line arguments (sent by the exim4 transport) to variables.
-$mail_username = $argv[1];
-$mail_domain = $argv[2];
- 
-foreach ($sites as $domain => $drush_alias) {
+$mail_domain = $argv[1];
+
+// Load site info
+require_once('site_info.php');
+$sites = og_mailinglist_site_info();
+
+$post_url = "";
+$validation_string = "";
+foreach ($sites as $domain => $info) {
   if (strtolower($mail_domain) === $domain) {
-    // Save the email to file. We would just pass the email directly through
-    // bash but as it turns out, there's a size limit to how big bash arguments
-    // can be and emails with large attachment's exceed that limit.
-    $rand_filename = "/tmp/" . rand(1000, 10000000);
-    file_put_contents($rand_filename, $raw_email . "\n");
-    exec("drush " . $drush_alias . " ogm-post-email " . $rand_filename . " " . $mail_username,
-         $result);
+    $post_url = $info['post_url'];
+    $validation_string = $info['validation_string'];
   }
 }
+
+if (empty($post_url)) {
+  echo "Could not match the email domain with a Drupal site. Check that you've setup site_info.php correctly.";
+  exit();
+}
+
+$token = md5($validation_string . $raw_email);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, FALSE);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_HEADER, 1);
+curl_setopt($ch, CURLOPT_URL, $post_url);
+
+//prepare the field values being posted to the service
+$data = array(
+  'message' => $raw_email,
+  'token' => $token,
+);
+
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+//make the request
+curl_exec($ch);
