2c203ac42eea73399f7be373fcf3444f54b0a124
Mattias Põldaru <mahfiaz@gmail.com>
2012-08-12 17:25:19 +0300

    by mahfiaz: Changed, usage of shell script style comments (starting with #) is discouraged in Drupal, use // instead.

--- migrate_notifications_og_mailinglist.php
+++ migrate_notifications_og_mailinglist.php
@@ -7,27 +7,29 @@
  *  welcome.
  */
 
-# set up the drupal directory -- very important
+// Set up the drupal directory -- very important.
 $DRUPAL_DIR = '/var/www/island_prod';
 
-# set some server variables so Drupal doesn't freak out
+// Set some server variables so Drupal doesn't freak out.
 $_SERVER['SCRIPT_NAME'] = '/script.php';
 $_SERVER['SCRIPT_FILENAME'] = '/script.php';
 $_SERVER['HTTP_HOST'] = 'example.com';
 $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
 $_SERVER['REQUEST_METHOD'] = 'POST';
 
-# act as the first user
+// Act as the first user.
 global $user;
 $user->uid = 1;
 
-# gain access to drupal
-chdir($DRUPAL_DIR);  # drupal assumes this is the root dir
-error_reporting(E_ERROR | E_PARSE); # drupal throws a few warnings, so suppress these
-require_once('./includes/bootstrap.inc');
+// Gain access to drupal.
+// Drupal assumes this is the root dir.
+chdir($DRUPAL_DIR);
+// Drupal throws a few warnings, so suppress these.
+error_reporting(E_ERROR | E_PARSE);
+require_once './includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 
-# restore error reporting to its normal setting
+// Restore error reporting to its normal setting.
 error_reporting(E_ALL);
 
 // Push all subsriptions in og_uid to og_mailinglist_group_subscription.
