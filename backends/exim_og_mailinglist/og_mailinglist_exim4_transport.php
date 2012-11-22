ee595679b612dced3665517f2612c6bbf7abff08
Mattias Põldaru <mahfiaz@gmail.com>
2012-11-22 18:48:26 +0200

    Issue #1341872 by mahfiaz: Fixed 302 redirect with CURL POST, now by default MTA pipes directly to bootstrapped Drupal and does not use example.com/og_mailinglist address anymore. This should perform better and makes it possible to shut down listener on example.com/og_mailinglist. The problem arose with Open Atrium, which for private use redirects EVERYTHING to login page. It also adds default.site_config.php file and duplicates og_mailinglist_debug.sh which can be found under postfix backend.

--- backends/exim_og_mailinglist/og_mailinglist_exim4_transport.php
+++ backends/exim_og_mailinglist/og_mailinglist_exim4_transport.php
@@ -3,49 +3,77 @@
 
 $raw_email = "";
 
-// Reads in raw email off the STDIN and posts the email using our curl command for the appropriate site.
+// Reads in raw email off the STDIN and posts the email using our curl command
+// for the appropriate site.
 $fd = fopen("php://stdin", "r");
 while (!feof($fd)) {
   $raw_email .= fread($fd, 1024);
 }
 
 // Set command line arguments (sent by the exim4 transport) to variables.
-$mail_domain = $argv[1];
+$mail_domain = strtolower($argv[1]);
 
-// Load site info
-require_once('site_info.php');
+// Load site info.
+require_once('/etc/exim4/exim_og_mailinglist/site_info.php');
 $sites = og_mailinglist_site_info();
 
-$post_url = "";
-$validation_string = "";
-foreach ($sites as $domain => $info) {
-  if (strtolower($mail_domain) === $domain) {
-    $post_url = $info['post_url'];
-    $validation_string = $info['validation_string'];
-  }
+if (empty($sites[$mail_domain])) {
+  echo "Could not match the email domain with a Drupal site. ";
+  echo "Check that you've setup site_info.php correctly.";
+  exit(1);
 }
 
-if (empty($post_url)) {
-  echo "Could not match the email domain with a Drupal site. Check that you've setup site_info.php correctly.";
-  exit();
+// Here we expect that all required elements are present in that array.
+$site = $sites[$mail_domain];
+
+if ($site['direct_posting'] === True) {
+  // We bootstrap Drupal now.
+  // Set some server variables so Drupal doesn't freak out.
+  $_SERVER['SCRIPT_NAME'] = '/og_mailinglist_transport.php';
+  $_SERVER['SCRIPT_FILENAME'] = '/og_mailinglist_transport.php';
+  $_SERVER['HTTP_HOST'] = $site['host'];
+  $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+  $_SERVER['REQUEST_METHOD'] = 'POST';
+
+  // Change to the Drupal directory.
+  chdir($site['drupal_path']);
+
+  // Silence errors during bootstrap.
+  //error_reporting(E_ERROR | E_PARSE);
+
+  // Run the initial Drupal bootstrap process.
+  require_once('includes/bootstrap.inc');
+  drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+
+  // Restore error reporting to its normal setting.
+  error_reporting(E_ALL);
+
+  // Include og_mailinglist_transport.inc file.
+  module_load_include('inc', 'og_mailinglist', 'og_mailinglist_transport');
+
+  // Finally post the email.
+  _og_mailinglist_process_email($raw_email);
+}
+else {
+  // Let's post over HTTP using Curl.
+  $token = md5($site['validation_string'] . $raw_email);
+
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, FALSE);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:'));
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_HEADER, 1);
+  curl_setopt($ch, CURLOPT_URL, $site['post_url']);
+
+  // Prepare the field values being posted to the service.
+  $data = array(
+    'message' => $raw_email,
+    'token' => $token,
+  );
+
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+  // Make the request.
+  curl_exec($ch);
 }
 
-$token = md5($validation_string . $raw_email);
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, FALSE);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:'));
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_HEADER, 1);
-curl_setopt($ch, CURLOPT_URL, $post_url);
-
-//prepare the field values being posted to the service
-$data = array(
-  'message' => $raw_email,
-  'token' => $token,
-);
-
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-
-//make the request
-curl_exec($ch);
